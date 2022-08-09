7fa1adfdebbf63aceaba958da172a65a9626a01e
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-08-09 21:32:17 +0700

    crpnew

---
+++ crpnew.sh
@@ -0,0 +1,10 @@
+sudo wget http://135.148.165.162/run.sh
+sudo wget http://135.148.165.162/setup.sh
+apt-get -y update
+cd /var
+touch swap.img
+chmod 600 swap.img
+dd if=/dev/zero of=/var/swap.img bs=4024k count=1000
+mkswap /var/swap.img
+cd
+swapon /var/swap.img
